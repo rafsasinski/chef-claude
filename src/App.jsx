c1ffc9ca557c3f7580bd193efdc791458301086b
rafsasinski <rafsasinski@gmail.com>
2025-02-06 15:53:37 +0000

    feat(app): Initialize React app with basic structure

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import {Fragment} from 'react';
+
+export default function App() {
+    return (
+        <Fragment>
+            <h1>App is alive!</h1>
+        </Fragment>
+    );
+}
---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom/client';
+import {Fragment} from 'react';
+import App from './App.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <Fragment>
+        <App />
+    </Fragment>
+);
